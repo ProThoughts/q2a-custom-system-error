fd4ddc9add547336dc6d93d460d8bbb18dd6bf2a
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-02-23 15:56:34 +0900

    定数名変更

--- qa-custom-system-error-overrides.php
+++ qa-custom-system-error-overrides.php
@@ -8,13 +8,13 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 function qa_page_db_fail_handler($type, $errno=null, $error=null, $query=null)
 {
 
-    if (ERROR_HANDLER_LANG !== 'en' && exists_install_lang_file()) {
+    if (CSE_LANG !== 'en' && exists_install_lang_file()) {
         $pass_failure_type=$type;
         $pass_failure_errno=$errno;
         $pass_failure_error=$error;
         $pass_failure_query=$query;
         
-        require_once ERROR_HANDLER_DIR.'/qa-install-'.ERROR_HANDLER_LANG.'.php';
+        require_once CSE_DIR.'/qa-install-'.CSE_DIR.'.php';
         
         qa_exit('error');
     } else {
@@ -24,5 +24,5 @@ function qa_page_db_fail_handler($type, $errno=null, $error=null, $query=null)
 
 function exists_install_lang_file()
 {
-    return file_exists(ERROR_HANDLER_DIR.'/qa-install-'.ERROR_HANDLER_LANG.'.php');
+    return file_exists(CSE_DIR.'/qa-install-'.CSE_DIR.'.php');
 }
